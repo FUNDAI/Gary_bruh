cb02bb51bbec1982f00604dfdb48b09ed01275d0
FUNDAI <ethan@jaiohsieh.com>
2023-03-04 16:50:49 +0800

    'bruh'

---
+++ Web_About/Js.js
@@ -0,0 +1,23 @@
+function changewordMac(){
+    
+    document.getElementById("p1").innerHTML="機械:和設計組密切討論，將設計組的草圖實現，製作機器人的零件與組裝。組裝完成後布置場地，並且排除測試後的故障，讓機器人順利運作。";
+    pic.src="../pictroll/pict6.png";
+}
+function changewordPro(){
+   
+    document.getElementById("p1").innerHTML="程式:使用Java語言根據自己團隊所設計的機器人做程式的編寫及設計，並且做優化及測試 ";
+    pic.src="../pictroll/pict5.png";
+}
+function changewordEle(){
+    
+    document.getElementById("p1").innerHTML=" 配電:配電就像機器人的神經系統一樣,讓機器人的每個部位能有動力,我們會幫助機械組做的機構配電讓機器人動起來, 平時會學習機器人的配電和氣動系統,也會和機械組討論機器人製作方向。 ";
+    pic.src="../pictroll/pict6.png";
+}
+function changewordDes(){
+    document.getElementById("p1").innerHTML=" 設計:分析競賽任務，將各個功能整合在空間有限的底盤上，藉由Solidworks畫出他們的想法，在組裝完成後反覆測試。  ";
+    pic.src="../pictroll/pict6.png";
+}
+function changewordWor(){
+    document.getElementById("p1").innerHTML=" 行政:行政組協助隊務推動,關心各組出席狀況並對會議記錄進行檢討, 以及承辦隊上活動,還有辦理活動和修訂團隊規章。" ;
+    pic.src="../pictroll/pict7.png";
+}
